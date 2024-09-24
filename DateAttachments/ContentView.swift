2eb350377c5bb7d01db238c0c2fc9162c690661d
Tim Mitra <tim@it-guy.com>
2024-09-24 01:36:27 -0400

    open attachments

--- DateAttachments/ContentView.swift
+++ DateAttachments/ContentView.swift
@@ -1,6 +1,6 @@
 //
 // ---------------------------- //
-// Original Project: DateAttachments
+// Original Project: Dates
 // Created on 2024-09-24 by Tim Mitra
 // Mastodon: @timmitra@mastodon.social
 // Twitter/X: timmitra@twitter.com
@@ -15,44 +15,48 @@ import RealityKitContent
 
 struct ContentView: View {
 
-    @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+    @State private var objects: [Date:Entity] = [:]
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
-                .font(.title)
-                .frame(width: 360)
-                .padding(24)
-                .glassBackgroundEffect()
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
+            RealityView { content, attachments in
+               if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                    content.add(scene)
+                }
+            } update: { content, attachments in
+                
+                objects.keys.forEach { key in
+                    
+                    if let entity = objects[key],
+                        let attachment = attachments.entity(for: key) {
+                        content.add(entity)
+                        entity.addChild(attachment)
                     }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
+                }
+            } attachments: {
+                ForEach(Array(objects.keys), id: \.self) { key in
+                    Attachment(id: key) {
+                        Text(key.description)
+                    }
                 }
             }
+            
+            VStack {
+                Button(action: {
+                    let entity = Entity()
+                    entity.setPosition([
+                        0,-0.20 + (Float(objects.count) * 0.025), 0
+                    ], relativeTo: nil)
+                    objects[Date.now] = entity
+                }, label: {
+                    Text("Insert Object!")
+                })
+                
+            }
+
+
         }
+        .padding()
     }
 }
 
