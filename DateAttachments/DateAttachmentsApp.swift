c74c111ebebe4f4d5242cd9d87924ee8d2a83804
Tim Mitra <tim@it-guy.com>
2024-09-24 01:32:48 -0400

    Initial Commit

---
+++ DateAttachments/DateAttachmentsApp.swift
@@ -0,0 +1,25 @@
+//
+// ---------------------------- //
+// Original Project: DateAttachments
+// Created on 2024-09-24 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct DateAttachmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
